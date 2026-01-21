9b6a978023f1a3a97bdfe496081b9ca0f3a08665
Kalyan Jc <156936170+w0rtyy@users.noreply.github.com>
2026-01-21 10:42:06 +0530

    database.py

--- fixed/app/database.py
+++ fixed/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://jckalyan@localhost/fastapi_app"
+DATABASE_URL = "postgresql://username@localhost/fastapi_app"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
